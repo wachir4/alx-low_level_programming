2a4f1a7bd58f0857933e2d5d7ccea40c72d5d496
Your Name <gmw24537@gmail.com>
2023-08-10 13:22:26 -0700

    echo

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:0
+ */
+int main(void)
+{
+printf("size of a char: %d byte(s)\n", size of(char));
+printf("size of an int: %d byte(S)\n", size of(int));
+printf("size of a long int: %d byte(s)\n", size of(long int));
+printf("size of a long long int: %d byte(s)\n", size of(long long int));
+printf("size of a float: %d byte(s)\n", size of(float));
+return (0);
+}
