8640b7b53963018e31adc400a1c1df5a3ef3c2da
Your Name <gmw24537@gmail.com>
2023-08-10 12:47:29 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main prints out a string to stdout.
+ * return 0 if success.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
