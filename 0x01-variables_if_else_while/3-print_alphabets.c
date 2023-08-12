517bf0a2e46044d4277c7e254d1849307b9de259
Your Name <gmw24537@gmail.com>
2023-08-12 02:06:09 -0700

    ./3-print_alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -this is the entry point.
+ * return : gives 0 = success
+ */
+int main(void)
+{
+char c;
+char d;
+c = 'a';
+d = 'A';
+while
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
+while
+(d >= 'Z')
+{
+putchar (d);
+d++;
+}
+putchar('\n');
+return (0);
+}
