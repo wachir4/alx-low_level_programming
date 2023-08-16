f5ba1c4d797e33d8e7749790ebb67e27c6a9dd49
Your Name <gmw24537@gmail.com>
2023-08-16 10:31:23 -0700

    ./5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints a sign or number
+ * @n- The number to be checked
+ * Return: 1 for positive num, -1 for nagative num or zero for others
+ */
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
