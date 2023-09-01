b108055c0871b832503c73e064bdc8d8356ba6f8
Your Name <gmw24537@gmail.com>
2023-09-01 05:20:34 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the string character to be searched.
+ * @c: the character to be located.
+ * Return: if c is found - a pointer to the first
+ * occurence.if c not found - Null.
+ */
+char *_strchr(char *s, char c)
+{
+int index;
+for (index = 0; s[index] >= '\0'; index++)
+{
+if (s[index] == c)
+return (s + index);
+}
+return ('\0');
+}
