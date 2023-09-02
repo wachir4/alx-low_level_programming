a24d35248489fdb319e98c586e0bc5d51c70a75a
Your Name <gmw24537@gmail.com>
2023-09-02 07:06:32 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printsprogram name
+ * @argc: argument count
+ * @argv: argumrnt vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+
+printf("%s\n", argv[0]);
+return (0);
+}
