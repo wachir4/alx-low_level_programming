f67d1d2c6d8241eee09857e1e8f78b0ae3bad7d1
Your Name <gmw24537@gmail.com>
2023-09-06 09:47:42 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,32 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
 /**
- * _strdup - returns a pointer to a new string which is
- * a duplicate of the string str
- * @str: string to be copied
- * Return: pointer to new memory
+ * str_concat - concatenates 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL on failure
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-int *copy;
-char count = 0;
-if (str == NULL)
+int s1len = 0;
+int s2len = 0;
+int i;
+char *output;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+s1len++;
+for (i = 0; s2[i] != '\0'; i++)
+s2len++;
+output = malloc(sizeof(char) * (s1len + s2len) +1);
+if (output == NULL)
 return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-count++;
-copy = malloc(sizeof(char) * count + 1);
-if (copt == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-copy[i] = str[i];
-return (copy);
+for (i = 0; s1[i] != '\0'; i++)
+output[i] = s1[i];
+for (i = 0; s2[i] != '\0'; i++)
+output[s1len + i] = s2[i];
+return (output);
 }
