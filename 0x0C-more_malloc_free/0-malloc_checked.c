b87323182783db76dee6427e7e5470663e59676a
Your Name <gmw24537@gmail.com>
2023-09-18 01:16:41 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - alocates memory by malloc
+ * @b: size of memory
+ * Return: pointer to new allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+char *output;
+output = malloc(b);
+if (output == NULL)
+exit(98);
+else
+return (output);
+}
